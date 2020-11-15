226e0b25418d1892eb43ce0590a8a5905cc5d815
jdavidrice <jdavidrice@gmail.com>
2020-11-14 23:01:18 -0500

    final version

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-## JavaScript Password Generator
+## Password Generator
 
 ## Description 
 
-This website is a JavaScript Password Generator. It allows the user to choose from these options: lowercase letters, uppercase letters, numbers, and special characters. The user also needs to specify the length of the password that is generated (8 - 128 characters). 
+This website is a Password Generator. It allows the user to choose from these options: lowercase letters, uppercase letters, numbers, and special characters. The user also needs to specify the length of the password that is generated (8 - 128 characters). 
 
 [Link to published website](https://jdavidrice.github.io/Password_Generator_2/)
 
@@ -23,47 +23,47 @@ This website is a JavaScript Password Generator. It allows the user to choose fr
 
 ![Screenshot of completed website.](amazing_pw_gen.png)
 
-* [Return to Top](#Javascript-Password-Generator)
+* [Return to Top](#Password-Generator)
 
 ## Installation
 
 There is nothing to install to utilize this Password Generator. 
 
-* [Return to Top](#Javascript-Password-Generator)
+* [Return to Top](#Password-Generator)
 
 ## Usage 
 
 This Password Generator can be used to generate passwords. 
 
-* [Return to Top](#Javascript-Password-Generator)
+* [Return to Top](#Password-Generator)
 
 ## Credits
 
 JavaScript file based on video from Traversy Media. [JavaScript Password Generator](https://www.youtube.com/watch?v=duNmhKgtcsI)
 
-* [Return to Top](#Javascript-Password-Generator)
+* [Return to Top](#Password-Generator)
 
 ## License
 
 No licenses used or implied 
 
-* [Return to Top](#Javascript-Password-Generator)
+* [Return to Top](#Password-Generator)
 
 ## Badges
 
 No badges 
 
-* [Return to Top](#Javascript-Password-Generator)
+* [Return to Top](#Password-Generator)
 
 ## Contributing
 
 Jeremy Rice
 
-* [Return to Top](#Javascript-Password-Generator)
+* [Return to Top](#Password-Generator)
 
 ## Tests
 
 No tests
 
-* [Return to Top](#Javascript-Password-Generator)
+* [Return to Top](#Password-Generator)
 
--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
-
 const randomFunc = {
     upper: getRandomUpper,
     lower: getRandomLower,
@@ -15,11 +14,11 @@ const randomFunc = {
     symbol: getRandomSymbol
 };
 
-// Generate event listen
+// Generate event listener
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value;
         if (length < "8" || length > "128") {
-            alert ('Please choose a password length between 8 and 128!')
+            alert ("Please choose a password length between 8 and 128.")
             window.location.reload()
             return ''    
         };    
@@ -27,8 +26,6 @@ generateEl.addEventListener('click', () => {
     const hasLower = lowercaseEl.checked;
     const hasNumber = numbersEl.checked;
     const hasSymbol = symbolsEl.checked;
-
-
     resultEl.innerText = generatePassword(
         hasUpper, 
         hasLower, 
@@ -42,79 +39,64 @@ generateEl.addEventListener('click', () => {
 function generatePassword(upper, lower, number, symbol, length) {
     // 1. Init pw var
     let generatedPassword = '';
-
     const typesCount = lower + upper + number + symbol;
-
     // console.log('typesCount: ', typesCount);
-
     // 2. Filter out unchecked types
     const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter
     (
         item => Object.values(item)[0]
     );
-
     // console.log('typesArr: ', typesArr);
-
     // If no boxes are checked, it won't generate a password
     if(typesCount === 0) {
+        alert ("Please check which character types you want to use.");
         return '';
     }
-
     // 3. Loop over length call generator function for each type
     for(let i = 0; i < length; i += typesCount) {
         typesArr.forEach(type => {
             const funcName = Object.keys(type)[0];
-
             // console.log('funcName: ', funcName);
-            
             generatedPassword += randomFunc[funcName]();
         });
     }
     // 4. Add final password to the password variable and return
     const finalPassword = generatedPassword.slice(0, length);
-
     return finalPassword;
 }
 
 // Generator functions
 function getRandomLower() {
-    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
     return lowercase[Math.floor(Math.random() * lowercase.length)];
 }
-
 function getRandomUpper() {
-    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     return uppercase[Math.floor(Math.random() * uppercase.length)];
 }
-
 function getRandomNumber() {
-    const number = '0123456789';
+    const number = "0123456789";
     return number[Math.floor(Math.random() * number.length)];
 }
-
 function getRandomSymbol() {
-    const symbols = '!"#$%&()*+,-./:;<=>?@[]^_`{|}~'
+    const symbols = "!'#$%&()*+,-./:;<=>?@[]^_`{|}~";
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
 // console.log(getRandomLower(), getRandomUpper(), getRandomNumber(), getRandomSymbol());
-
 
 // Copy password to clipboard
 clipboardEl.addEventListener('click', () => {
     const textarea = document.createElement('textarea');
     const password = resultEl.innerText;
-
     if(!password) {
-        alert ("Please Generate a Password!")
+        alert ("Please generate a password before you copy.")
         return;
     }
-
     textarea.value = password;
     document.body.appendChild(textarea);
     textarea.select();
     document.execCommand('copy');
     textarea.remove();
-    alert('Password copied to clipboard!');
+    alert('Password copied to clipboard.');
     window.location.reload();
 });
